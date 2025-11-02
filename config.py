f86a0704bf5ef314b57be2a1888b4635b056b005
Nikita Zolin <mrdelrus@yandex.ru>
2025-11-02 13:29:31 +0400

    removed config file

--- config.py
+++
@@ -1,6 +0,0 @@
-import pathlib
-
-PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent
-DATA_DIR = PROJECT_ROOT / "data"
-MODELS_DIR = PROJECT_ROOT / "models"
-LOGS_DIR = PROJECT_ROOT / "logs"
