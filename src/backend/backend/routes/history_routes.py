132ca5be9285059c776a97af6274428729e67b7a
Nikita Zolin <mrdelrus@yandex.ru>
2025-12-26 00:02:33 +0400

    initial database attempt

---
+++ src/backend/backend/routes/history_routes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.db import get_db
+from backend.models.history import History
+from core.schemas.api.history import HistoryItem, HistoryResponse
+
+router = APIRouter(prefix="/history", tags=["history"])
+
+
+@router.get("", response_model=HistoryResponse)
+async def get_history(
+    limit: int = Query(5, ge=1, le=20),
+    db: AsyncSession = Depends(get_db),
+) -> HistoryResponse:
+    stmt = select(History).order_by(History.timestamp.desc()).limit(limit)
+    result = await db.execute(stmt)
+    rows: list[History] = result.scalars().all()
+
+    history_items: list[HistoryItem] = [HistoryItem.from_orm(row) for row in rows]
+
+    return HistoryResponse(history=history_items)
--- src/backend/backend/routes/models_routes.py
+++ src/backend/backend/routes/models_routes.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from uuid import uuid4
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.config import settings
+from backend.db import get_db
+from backend.models.history import History
 from backend.utils.s3_loader import list_models
 from core.schemas.api.models import ModelItem, ModelListResponse
 
@@ -8,10 +13,26 @@ router = APIRouter(prefix="/models", tags=["models"])
 
 
 @router.get("", response_model=ModelListResponse)
-async def get_models() -> ModelListResponse:
-    models = list_models()
-    cleaned_models = [
-        ModelItem(name=model.replace(f".{settings.MODEL_EXTENSION}", ""))
-        for model in models
-    ]
-    return ModelListResponse(models=cleaned_models)
+async def get_models(
+    db: AsyncSession = Depends(get_db),
+) -> ModelListResponse:
+    query_id = uuid4()
+    http_status = status.HTTP_200_OK
+
+    try:
+        models = list_models()
+        cleaned_models = [
+            ModelItem(name=model.replace(f".{settings.MODEL_EXTENSION}", ""))
+            for model in models
+        ]
+        return ModelListResponse(models=cleaned_models)
+
+    finally:
+        db.add(
+            History(
+                query_id=query_id,
+                endpoint="/models",
+                code_status=http_status,
+            )
+        )
+        await db.commit()
