0140e45e9bd8dbdd5737db8ffa531d9d9cb592e4
Nikita Zolin <mrdelrus@yandex.ru>
2025-12-25 23:41:13 +0400

    adjusted schemas for database pull request

--- src/core/schemas/api/history.py
+++ src/core/schemas/api/history.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 from typing import List
+from uuid import UUID
 
 from pydantic import BaseModel
 
 
 class HistoryItem(BaseModel):
-    username: str
-    query_name: str
-    code_name: str
+    query_id: UUID
+    endpoint: str
+    code_status: int
     timestamp: datetime
+
+    class Config:
+        orm_mode = True
 
 
 class HistoryResponse(BaseModel):
