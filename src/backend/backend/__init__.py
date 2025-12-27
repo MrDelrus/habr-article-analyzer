02f3f392b35541ca84d8819b34c970ff4179a6c3
Nikita Zolin <mrdelrus@yandex.ru>
2025-12-28 00:52:02 +0400

    fix imports

--- src/backend/backend/__init__.py
+++ src/backend/backend/__init__.py
@@ -1,4 +1,4 @@
-from config import settings
-from db import get_db
+from backend.config import settings
+from backend.db import get_db
 
 __all__ = ["settings", "get_db"]
--- src/backend/backend/config.py
+++ src/backend/backend/config.py
@@ -2,7 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    AWS_REGION: str
     DATABASE_URL: str
     ML_SERVICE_URL: str
     INTERNAL_API_KEY: str
