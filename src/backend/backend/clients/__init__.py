efe773e47b2dae06ba2b2148b35c76f7afb60603
Nikita Zolin <mrdelrus@yandex.ru>
2026-01-04 17:30:31 +0400

    fixed path in __init__.py

--- src/backend/backend/clients/__init__.py
+++ src/backend/backend/clients/__init__.py
@@ -1,5 +1,5 @@
-from database_client import DatabaseClient, get_database_client
-from ml_service_client import MLServiceClient, get_ml_service_client
+from backend.clients.database_client import DatabaseClient, get_database_client
+from backend.clients.ml_service_client import MLServiceClient, get_ml_service_client
 
 __all__ = [
     "get_ml_service_client",
